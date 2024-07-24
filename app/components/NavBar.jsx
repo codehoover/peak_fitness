03e45488b678e24f73dba1e664e720be695bddeb
Esau R <mumbonumber7@gmail.com>
2024-07-24 11:43:12 -0500

    restructured file heirarchy to enable page routing in app folder

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,37 +1,33 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Peak_img from '../assets/peak_lm.png'
-import ContactUs from '../pages/ContactUs';
 
 export default function NavBar(){
     return(
         <div className='flex flex-row justify-between px-4 bg-neutral-800 w-full fixed'>
             <section className='flex flex-row text-orange-500 items-center'>
-                <Image src={Peak_img} width={75} height={75}/>
-                <h1 className='text-sm font-bold'> Peak Fitness. </h1>
+                <Image src={Peak_img} width={65} height={65} alt='Peak fitness logo' priority/>
+                <h1 className='text-sm font-bold'> Peak Fitness </h1>
             </section>
 
             <section className='flex flex-row gap-4 items-center text-orange-500 font-semibold'>
-                <div> 
-                    Home
-                </div>
-                <div>
-                    About
-                </div>
+                <Link href='/'> Home </Link>
+                
+                <Link href="/about"> About </Link>
 
-                <div>
-                    Contact 
-                </div>
+                <Link href='/contact'> Contact </Link>
             </section>
 
             <section className='flex flex-row items-center gap-4'>
-                <div className='text-orange-500 border-2 border-orange-500 p-2 px-4 text-sm rounded-lg font-bold'>
+                <Link className='text-orange-500 border-2 border-orange-500 p-2 px-4 text-sm rounded-lg font-bold'
+                href='/login'>
                     Login
-                </div>
+                </Link>
 
-                <div className='bg-orange-500 rounded-lg text-sm font-bold p-2.5 px-4'>
+                <Link className='bg-orange-500 rounded-lg text-sm font-bold p-2.5 px-4'
+                href='/register'>
                     Sign Up
-                </div>
+                </Link>
 
             </section>
 
--- app/page.js
+++ app/page.js
@@ -1,15 +1,10 @@
 import Image from "next/image";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Tracker from "./components/Tracker";
 import NavBar from "./components/NavBar";
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
       <NavBar />
-      <Register />
-      <Login />
-      <Tracker />
     </main>
   );
 }
