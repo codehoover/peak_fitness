6c669d97282bf080c1f9a5404dda3499a98875d2
Esau R <mumbonumber7@gmail.com>
2024-07-30 18:50:51 -0500

    fixed navigation issue by using usePathname hook

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,12 +1,16 @@
 'use client';
-
+import {  useState } from 'react';
+import { useRouter,usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import Peak_img from '../assets/peak_lm.png'
-import { useState } from 'react';
 
-export default function NavBar(args){
-    const [isActive, setIsActive] = useState(0)
+export default function NavBar(){
+    const router = useRouter();
+    
+    //reads current urls pathname
+    const pathname = usePathname();
+
     return(
         <nav className={'flex flex-row justify-between px-4 bg-transparent w-full fixed z-50'}>
             <Link className='flex flex-row text-orange-500 items-center '
@@ -17,29 +21,35 @@ export default function NavBar(args){
             </Link>
 
             <section className='grid grid-cols-3 gap-4 place-items-center text-orange-500 font-semibold'>
-                <Link className={isActive == 0 ? "border-b-2 border-orange-500 duration-150": "hover:-translate-y-0.5 hover:font-normal duration-300 font-light border-none"}
-                href='/'
-                onClick={()=> setIsActive(0)}> Home </Link>
-                
-                <Link className={isActive == 1 ? "border-b-2 border-orange-500 duration-150": "hover:-translate-y-0.5 hover:font-normal duration-300 font-light border-none"}
-                href="/about"
-                onClick={()=> setIsActive(1)}> About </Link>
+                <Link href={'/'} 
+                onClick={() => router.push('/')}
+                className={pathname == '/' ? "border-b-2 border-orange-500 duration-150": "hover:-translate-y-0.5 hover:font-normal duration-300 font-light border-none"}>
+                    Home
+                </Link>
 
-                <Link className={isActive == 2 ? "border-b-2 border-orange-500 duration-150": "hover:-translate-y-0.5 hover:font-normal duration-300 font-light border-none"}
-                href='/contact'
-                onClick={()=> setIsActive(2)}> Contact </Link>
+                <Link href={'/about'} 
+                onClick={() => router.push('/about')}
+                className={pathname == '/about' ? "border-b-2 border-orange-500 duration-150": "hover:-translate-y-0.5 hover:font-normal duration-300 font-light border-none"}>
+                    About
+                </Link>
+
+                <Link href={'/contact'} 
+                onClick={() => router.push('/contact')}
+                className={pathname == '/contact' ? "border-b-2 border-orange-500 duration-150": "hover:-translate-y-0.5 hover:font-normal duration-300 font-light border-none"}>
+                    Contact
+                </Link>
             </section>
 
             <section className='flex flex-row items-center gap-4'>
                 <Link className='text-orange-500 border-2 border-orange-500 p-2 px-4 text-sm rounded-lg font-bold hover:bg-orange-500 hover:text-neutral-800 duration-300'
                 href='/login'
-                onClick={()=> setIsActive(4)}>
+                >
                     Login
                 </Link>
 
                 <Link className='bg-orange-500 rounded-lg text-sm font-bold p-2.5 px-4 hover:bg-orange-600 duration-300'
                 href='/register'
-                onClick={()=> setIsActive(5)}>
+                >
                     Sign Up
                 </Link>
 
